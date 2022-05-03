be923c17334b4db1eb6dfb40156ca06addcfdfe3
Gustavo Marcelo Nuñez <guscostaf@gmail.com>
2022-05-03 02:26:31 -0300

    Change password visibility OK

--- app/src/main/java/com/example/demo/LoginFragment.kt
+++ app/src/main/java/com/example/demo/LoginFragment.kt
@@ -1,23 +1,30 @@
 package com.example.demo
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
+import android.view.View.OnTouchListener
 import android.view.ViewGroup
 import android.view.animation.AlphaAnimation
-import androidx.compose.animation.fadeOut
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.demo.databinding.FragmentLoginBinding
 
+
+private var visible: Boolean = false
 
 class LoginFragment : Fragment() {
 
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -27,7 +34,39 @@ class LoginFragment : Fragment() {
 
         _binding!!.loginButton.setOnClickListener{ checkLogin() }
         _binding!!.cancelButton.setOnClickListener{ goBack() }
+
+        _binding!!.editTextPassword.setOnTouchListener(OnTouchListener { v, event ->
+            val DRAWABLE_LEFT = 0
+            val DRAWABLE_TOP = 1
+            val DRAWABLE_RIGHT = 2
+            val DRAWABLE_BOTTOM = 3
+            if (event.action == MotionEvent.ACTION_UP) {
+                if (event.rawX >= _binding!!.editTextPassword.getRight() - _binding!!.editTextPassword.getCompoundDrawables()
+                        .get(DRAWABLE_RIGHT).getBounds().width()
+                ) {
+                    turnVisibility()
+                    return@OnTouchListener true
+                }
+            }
+            false
+        })
+
+
+
         return view
+    }
+
+    private fun turnVisibility() {
+        if(!visible) {
+            _binding!!.editTextPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+            _binding!!.editTextPassword.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.visible_on, 0);
+            visible = true;
+        }
+        else {
+            _binding!!.editTextPassword.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+            _binding!!.editTextPassword.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.visible_off, 0);
+            visible = false;
+        }
     }
 
     private fun checkLogin () {
@@ -52,7 +91,7 @@ class LoginFragment : Fragment() {
     }
 
     private fun checkPassword(): Boolean {
-        if (_binding?.editTextTextPassword?.text.toString() == "admin")
+        if (_binding?.editTextPassword?.text.toString() == "admin")
             return true;
         return false;
     }
